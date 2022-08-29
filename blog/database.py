38b896c2b1645df51754c38d06f341bc8aa4c5c1
Satya Siva Chandrahasa <65844280+chandra-hasa@users.noreply.github.com>
2022-08-29 16:26:26 +0530

    Update database.py

--- blog/database.py
+++ blog/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:777chandra@localhost:5432/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:******@localhost:5432/****"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
